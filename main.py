3994ef03c2c59a05b6841885d41ffbb03f2df6e8
Kasia Łuczak <210kasia@wp.pl>
2022-05-29 17:02:34 +0200

    4

--- main.py
+++ main.py
@@ -2,13 +2,10 @@ class Biblioteka:
  dostepne_ksiazki=[]
  def __init__(self):
   self.ksiazka=dict()
-
+ 
  def dodaj_egzemplarz_ksiazki(self, tytul, autor, rok_wydania):
   self.dostepne_ksiazki.append(tytul)
   return True
- 
- def __pobierz_czytelnika(self, nazwisko):
-  czytelnik=Czytelnik()
  def wypozycz(self, nazwisko, tytul):
   czytelnik=Czytelnik()
   return czytelnik.wypozycz(nazwisko,tytul)
@@ -23,7 +20,7 @@ class Czytelnik(Biblioteka):
  oddane=[]
  def __init__(self):
   self.nazwisko=dict()
- def wypozycz(self, nazwisko, tytul):
+ def wypozycz(self, nazwisko,tytul):
   self.wypozyczone_ksiazki.append(nazwisko)
   self.lista_wypozyczonych_ksiazek.append((nazwisko,tytul))
   lista_nazwisk=set(self.wypozyczone_ksiazki)
@@ -65,9 +62,3 @@ biblioteka = Biblioteka()
 czytelnik = Czytelnik()
 for i in range(int(input())):
  t = eval(input())
- if t[0] == "dodaj":
-  print(biblioteka.dodaj_egzemplarz_ksiazki(t[1], t[2], t[3]))
- elif t[0] == "wypozycz":
-  print(biblioteka.wypozycz(t[1],t[2]))
- elif t[0]=="oddaj":
-  print(biblioteka.oddaj(t[1],t[2]))
